a8bbfdcb2d9cba2be94345463ba0f810f09598b5
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2023-07-13 10:03:27 +0630

    feat: profile card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="itw.png" alt="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Kyaw Zin Htet</h1>
+      <p>
+        Full-stack web developer currently working as a backend developer. When
+        not coding, I like to play guitar, read books, or watch movies.
+      </p>
+    </div>
+  );
+}
+
+function SkillList({ skill, emoji, bgColor }) {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML+CSS" emoji="💪" bgColor="#DD4B25" />
+      <Skill skill="JavaScript" emoji="💪" bgColor="#F7E018" />
+      <Skill skill="React" emoji="👍" bgColor="#149ECA" />
+      <Skill skill="Laravel" emoji="💪" bgColor="#E8503E" />
+      <Skill skill="Git and Github" emoji="👍" bgColor="#808080" />
+    </div>
+  );
+}
+
+function Skill({ skill, emoji, bgColor }) {
+  return (
+    <div className="skill" style={{ backgroundColor: bgColor }}>
+      {skill} {emoji}
+    </div>
+  );
+}
+
+ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
