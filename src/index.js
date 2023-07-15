70ddf86e20583ba33e062e26664bb344502f5e1b
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2023-07-15 21:40:17 +0630

    feat: skill list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,39 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import "./index.css";
 
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "intermediate",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Laravel",
+    level: "intermediate",
+    color: "#FF3B00",
+  },
+];
+
 function App() {
   return (
     <div className="card">
@@ -34,19 +67,22 @@ function Intro() {
 function SkillList() {
   return (
     <div className="skill-list">
-      <Skill skill="HTML+CSS" emoji="💪" bgColor="#DD4B25" />
-      <Skill skill="JavaScript" emoji="💪" bgColor="#F7E018" />
-      <Skill skill="React" emoji="👍" bgColor="#149ECA" />
-      <Skill skill="Laravel" emoji="💪" bgColor="#E8503E" />
-      <Skill skill="Git and Github" emoji="👍" bgColor="#808080" />
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} level={skill.level} bgColor={skill.color} />
+      ))}
     </div>
   );
 }
 
-function Skill({ skill, emoji, bgColor }) {
+function Skill({ skill, level, bgColor }) {
   return (
     <div className="skill" style={{ backgroundColor: bgColor }}>
-      {skill} {emoji}
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
     </div>
   );
 }
